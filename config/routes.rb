07d8155b731cd4a837367a0eb155774e37124596
Atekelt Afework <atekeltafework55@gmail.com>
2023-07-26 16:51:11 -0700

    register - login - edit added

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,10 @@
 Rails.application.routes.draw do
+  devise_scope :user do
+    # Redirests signing out users back to sign-in
+    get "users", to: "devise/sessions#new"
+    get '/users/sign_out' => 'devise/sessions#destroy'
+  end
+  devise_for :users
   root 'home#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
